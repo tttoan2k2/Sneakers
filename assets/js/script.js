f27b208975cbbee55899a40a4fca59fe86e88363
tttoan2k2 <boynhangheo2002hd@gmail.com>
2021-11-25 16:21:23 +0700

    wed Sneakers

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+// ============ Menu show ============
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu');
+
+// Remove menu
+const navLinks = document.querySelectorAll('.nav__link');
+const navMenu = document.getElementById('nav-menu');
+
+function linkAction() {
+    navMenu.classList.remove('show');
+}
+
+navLinks.forEach(navLink => navLink.addEventListener('click', linkAction));
+
+// ================ Scroll section active link ================================
+const sections = document.querySelectorAll('section[id]');
+
+window.addEventListener('scroll', scrollActive);
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href *='+ sectionId +']').classList.add('active');
+        } else {
+            document.querySelector('.nav__menu a[href *='+ sectionId +']').classList.remove('active');
+        }
+    })
+}
+
+//============== Change color header ================
+window.onscroll = () => {
+    const navHeader = document.getElementById('header');
+    if (this.scrollY >= 200) {
+        nav.classList.add('scroll-header');
+    } else {
+        nav.classList.remove('scroll-header');
+    }
+}
